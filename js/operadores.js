e148356c1bd1e4ce8a4d6ea1b0842050a43e789d
Soy-MartaGilC <marta.gil@bootcamp-upgrade.com>
2022-10-04 12:24:34 +0200

    Feat JS

---
+++ js/operadores.js
@@ -0,0 +1,25 @@
+
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+let multiplica = 10 * 5;
+console.log(multiplica)
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+ let divide = 10 / 2;
+ console.log(divide)
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+var firstNum = 15;
+var secondNum = 9;
+
+let resto = 15 % 9;
+console.log(resto)
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+//teniendo dos variables y = 10 y z = 5.
+var y = 10;
+var z = 5;
+
+var x = y + z;
+console.log(x)
+//1.5 Usa el correcto operador de asignación que resultará en x = 50,
+//teniendo dos variables y = 10 y z = 5.
+
+var x = y * z;
+console.log(x)
